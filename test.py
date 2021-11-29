31c286467aa12aea39a83ae531a54284c20dfcd9
zongzhicheng <842117866@qq.com>
2021-11-29 21:27:15 +0800

    test

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from conf import *
 
 
 
-pro = ts.pro_api(TS_TOKEN)
+# pro = ts.pro_api(TS_TOKEN)
 # print(pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date'))
-print(pro.new_share(start_date='20210906', end_date='20210920'))
+# print(pro.new_share(start_date='20210906', end_date='20210920'))
